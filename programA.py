c3cb8e7e551209eee9171548ea1a19fd22edf6ee
Rohan Keenoy <rohankeenoy@gmail.com>
2024-10-18 22:15:10 -0500

    some testing on initial tests

--- programA.py
+++ programA.py
@@ -9,8 +9,9 @@ class programA:
         self.listOfDirectoryFiles = []
         self.fileName = "ValidityChecks.txt"
         self.fileCreated = False
-        
-        
+        self.fileCheck = pd.DataFrame()
+        self.currentFile = ""
+    
         self.classDriver()
     #write to that file
     def writeToValidityFile(self, toWrite):
@@ -22,45 +23,71 @@ class programA:
         if self.fileCreated == False:
             with open(self.fileName, "w") as  validCheck:
                 self.fileCreated = True
+                
+    def initialFormatCheck(self):
+        if self.fileCheck.empty:
+            self.writeToValidityFile(f"FILE {self.currentFile}: CSV is empty.")
+            return 1
+        elif self.fileCheck.shape[0] < 2 or self.fileCheck.shape[1] < 2:
+            self.writeToValidityFile(f"FILE {self.currentFile}: Initial check for shape failed")
+            return 1
+        #A CHECK FOR THE FIRST LAST NAME
+        elif pd.isna(self.fileCheck.iloc[0,0]) or pd.isna(self.fileCheck.iloc[0,1]) or  (self.fileCheck.iloc[0, 0] == False or self.fileCheck.iloc[0, 1] == False) :
+            self.writeToValidityFile(f"FILE {self.currentFile}: Initial check for name failed")
+            return 1
+        #acheck for CLASS NAME
+        elif pd.isna(self.fileCheck.iloc[1,0]) or self.fileCheck.iloc[1,0] != "CS 4500" :
+            self.writeToValidityFile(f"FILE {self.currentFile}: Initial check for CLASS name failed")
+            return 1
+        
+        else:
+            return 0
             
     #base check for fromat
     def openFilesAndCheck(self):
-        for file in range(len(self.listOfDirectoryFiles)):
-            try:
-                fileCheck = pd.read_csv(f"{file}")
-            except:
-                print("Could not open a supposedly valid csv: {file}, continuing")
-            if(fileCheck):
-                #creates the validity check file 
-                self.createFile()
+        #main loop for checking
+        for file in self.listOfDirectoryFiles:
+            checkedFailed = 0
+            self.currentFile = file
+            self.fileCheck = pd.read_csv(f"{file}",header = None, na_filter=False)
+            print(self.fileCheck)
+            self.createFile()
+            #self.initialFormatCheck(fileCheck)
+            checkedFailed = self.initialFormatCheck()
+            if checkedFailed == 1:
+                continue
+            
+                
+                
+            
+                
                 
                   
     #checks directory
     def fileChecks(self):
         #modified my old regex from phase A, tested on regex101.com. 
         #posted the image for doc in server
+        count = 0
         regPat = r"^[A-Za-z]+[lL][oO][gG]\.[cC][sS][vV]$"
         for file in os.listdir():
             if re.match(regPat,file):
                 count +=1
                 self.listOfDirectoryFiles.append(file)
-        if len(self.listOfDirectoryFiles) < 1:
+        print(f"LENGTH {len(self.listOfDirectoryFiles)}")
+        if len(self.listOfDirectoryFiles) <= 0:
             print("There are no valid files matching pattern: X (any capital letter) + [lL][oO][gG]\.[cC][sS][vV] \n EXITING.......")
             #had to put this in so when an executable is generated it doesn't quit extremely fast
             time.sleep(2)
             exit(1)
         print(f"Total valid file names found {count}\n With more than one file found, opening a new file called ValidityChecks.txt")
-        if len(self.listOfDirectoryFiles > 1):
+        if len(self.listOfDirectoryFiles)> 0:
             self.openFilesAndCheck()
         #sorta redundant, meh
-        else: 
-            print("There are no valid files matching pattern: X (any capital letter) + [lL][oO][gG]\.[cC][sS][vV] \n EXITING.......")
-            #had to put this in so when an executable is generated it doesn't quit extremely fast
-            time.sleep(2)
-            exit(1)
+       
         
     
     def classDriver(self):
+        
         print(self.introMessage)
         input("Press any key to continue....")
         self.fileChecks()
