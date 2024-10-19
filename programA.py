d26a2563549ca2905c6d3310004e1b6145149c14
Rohan Keenoy <rohankeenoy@gmail.com>
2024-10-18 20:26:57 -0500

    first check working, tested regex on regex101.com

--- programA.py
+++ programA.py
@@ -1,17 +1,40 @@
 import os
-
+import pandas as pd
+import re
+import time
 class programA:
     def __init__(self):
         self.introMessage = "---------Welcome to program A of phase II. This program is for validating patterns in file names and checks the validity of file contents. ---------\n"
-        
+        #data strucutre "L per assignment document"
+        self.listOfDirectoryFiles = []
         
         
         
         self.classDriver()
     
+    def fileChecks(self):
+        #modified my old regex from phase A, tested on regex101.com. 
+        #posted the image for doc in server
+        regPat = r"^[A-Za-z]+[lL][oO][gG]\.[cC][sS][vV]$"
+        for file in os.listdir():
+            if re.match(regPat,file):
+                count +=1
+                self.listOfDirectoryFiles.append(file)
+        if len(self.listOfDirectoryFiles) < 1:
+            print("There are no valid files matching pattern: X (any capital letter) + [lL][oO][gG]\.[cC][sS][vV] \n EXITING.......")
+            #had to put this in so when an executable is generated it doesn't quit extremely fast
+            time.sleep(2)
+            exit(1)
+        print(f"Total valid file names found {count}\n With more than one file found, opening a new file called ValidityChecks.txt")
+        
+        
+        
+        
+    
     def classDriver(self):
         print(self.introMessage)
         input("Press any key to continue....")
+        self.fileChecks()
             
 
 if __name__ == "__main__":
