ed4fbfaadd3e7fb25be69eddc9e19811962a1a75
Rohan Keenoy <rohankeenoy@gmail.com>
2024-10-24 18:23:11 -0500

    final program A?

--- programA.py
+++ programA.py
@@ -1,9 +1,43 @@
+"""
+Language: Python 3
+
+IDE: VS Code
+
+HOW TO RUN: Navigate to where the python project and execute as python {projectname}.py, because python is an interpreted language, an external library is used to "compile" it for an executable.
+    So you can either run the script or click on the executable. It was produced with a library called pyinstaller and ran with this command:
+     python c:.users.rohan.appdata.local.packages.pythonsoftwarefoundation.python.3.12_qbz5n2kfra8p0.localcache.local-packages.python312.site-packages.pyinstaller. --onefile {projectA}.py
+    Where the periods are forward slashes. (unicode parsing problem if leaving path in multi line comment)
+Authors: Rohan Keenoy, Justin Macapanpan
+
+Date: 10/24/2024
+
+DATA STRUCTURES: A pandas dataframe is commonly used in scientific applications, it can be thought of as a N-d array,can have headers, and is structured as the csv is structured. 
+    For this project using a dataframe is a no brainer. R has a similar structure. A dictionary is used to append to rows in a dataframe. This generated per row-entry.  An array called listOfDirectoryFiles is initalized as a class variable, this is a simple array that holds on to
+    files to traverse. This is the structure called "L" in the specification.
+    
+General Flow: basically just opperates straight down the checks with a flag variable called "checkFailed" in the function OpenFilesAndCheck, if a check is failed , it calls a function to write to file that it failed, 
+this does not happen on the exception of a warning though - it will just print a warning and set the flag back to 0, which is valid. It then continues. 
+
+EXTERNAL files: Any file that is valid is used in the directory, generated a ValidityChecks.txt file.
+
+External preperation: Because python is an interpreted language a software pyinstaller will be used to generate an executable. 
+    
+References:
+    0.)Regex testing on : https://regex101.com/
+    1.) Date checks: https://www.geeksforgeeks.org/python-validate-string-date-format/, 
+    format reference https://pynative.com/python-datetime-format-strftime/
+    2.) General Pandas referencing on documentation site: https://pandas.pydata.org/docs/getting_started/index.html
+    
+"""
+
+
 import os
 import pandas as pd
 import re
 import time
 from datetime import datetime
 class programA:
+    #insantiation of class variables
     def __init__(self):
         self.introMessage = "---------Welcome to program A of phase II. This program is for validating patterns in file names and checks the validity of file contents. ---------\n"
         #data strucutre "L per assignment document"
@@ -15,17 +49,17 @@ class programA:
         self.currentLine = 3
     
         self.classDriver()
-    #write to that file
+    #write to that file whatever is sent (successful, failure, warning messages, called from main program loop)
     def writeToValidityFile(self, toWrite):
         if self.fileCreated == True:
             with open(self.fileName, "a") as validCheck:
                 validCheck.write(f"{toWrite} . \n")
-    #create a file
+    #create a file, will not create a new file everytiem with fileCreatede flag
     def createFile(self):
         if self.fileCreated == False:
             with open(self.fileName, "w") as  validCheck:
                 self.fileCreated = True
-            
+    #initial format checking for each file in directory, checks the name and class columns
     def initialFormatCheck(self):
         if self.fileCheck.empty:
             self.writeToValidityFile(f"FILE {self.currentFile}: CSV is empty.")
@@ -43,11 +77,13 @@ class programA:
             return 1
         else:
             return 0
+    #check if there is aditional lines after the name and class name lines
     def checkForAdditionalLines(self):
         if self.fileCheck.shape[0] > 2:
             return 1
         return 0
     #https://www.geeksforgeeks.org/python-validate-string-date-format/
+    #date check 
     def dateCheck(self, date):
         #format reference https://pynative.com/python-datetime-format-strftime/
         format = "%m/%d/%Y"
@@ -61,21 +97,21 @@ class programA:
             return 1
         else:
             return 0
-        
+    #function to check for people involved column
     def checkPplInvolved(self,ppl):
         ppl = int(ppl)
         if ppl < 1 or ppl > 50:
             return 1
         else:
             return 0
-    
+    #activity code checking
     def checkActivityCode(self, activityCode):
         validCodes = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9", "A", "B", "C", "D"]
         if activityCode in validCodes:
             return 0
         else:
             return 1
-        
+    #note validity checking 
     def checkNote(self, activityCode, note):
         if activityCode == "D":
             if(len(note)< 1):
@@ -87,7 +123,7 @@ class programA:
         else:
             return 0
                  
-    #base check for fromat
+    #base check for fromat, this is the "main loop" basically if something fails, a flag is set and it breaks the flow 
     def openFilesAndCheck(self):
         print(self.listOfDirectoryFiles)
         #main loop for checking
@@ -133,8 +169,8 @@ class programA:
                 #check if time elapsed is greater than 4 hours
                 checkedFailed = self.overTimeCheck(timeCheck1, timeCheck2)
                 if checkedFailed == 1:
-                    self.writeToValidityFile(f"FILE {self.currentFile}: time elapsed exceeds 4 hours on line {self.currentLine}")
-                    break
+                    self.writeToValidityFile(f"********WARNING****** FILE {self.currentFile}: time elapsed exceeds 4 hours on line {self.currentLine}. Scan was continued for other checks")
+                    checkFailed = 0
                 #END JUSTIN 
                 pplInvolved = row[3]
                 checkedFailed = self.checkPplInvolved(pplInvolved)
@@ -156,8 +192,9 @@ class programA:
             if checkedFailed == 1:
                 continue
             
-            self.writeToValidityFile(f"FILE {self.currentFile}: SUCCESSFUL")
+            self.writeToValidityFile(f"FILE {self.currentFile}: SUCCESSFUL - Is a valid file.")
     #Justin Bloc   
+    #time check for making sure they are valid
     def timeCheck(self, t1, t2):
         #This only checks it for the HH:MM:SS format and assume military time
         format = "%H:%M"
@@ -182,6 +219,7 @@ class programA:
             return 0
     #End justin Block
     #checks directory
+    #actually retrieves and checks files initially from directory
     def fileChecks(self):
         #modified my old regex from phase A, tested on regex101.com. 
         #posted the image for doc in server
@@ -195,7 +233,7 @@ class programA:
         if len(self.listOfDirectoryFiles) <= 0:
             #print("There are no valid files matching pattern: X (any capital letter) + [lL][oO][gG]\.[cC][sS][vV] \n EXITING.......")
             #had to put this in so when an executable is generated it doesn't quit extremely fast
-            time.sleep(2)
+            input("No files found fitting the specification. Press enter to end program. . .")
             exit(1)
         print(f"Total valid file names found {count}\n With more than one file found, opening a new file called ValidityChecks.txt")
         if len(self.listOfDirectoryFiles)> 0:
@@ -203,12 +241,13 @@ class programA:
         #sorta redundant, meh
        
         
-    
+    #just a bit of a driver that runs all this on instantaition of the class
     def classDriver(self):
         
         print(self.introMessage)
-        input("Press any key to continue....")
+        input("Press enter key to continue....")
         self.fileChecks()
+        input(f"Thank you for using Program A. Validity check logs are in file {self.fileName}. \n Press enter to end the program (executable immediatly ends without this)")
             
 
 if __name__ == "__main__":
